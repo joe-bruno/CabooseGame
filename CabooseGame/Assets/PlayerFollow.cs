9dc275839284e92998756efb7dbb72d961e098fb
Joe Bruno <Joe.Bruno@panerabread.com>
2017-10-16 01:08:31 -0400

    Added player, movement, camera.

---
+++ CabooseGame/Assets/PlayerFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour {
+
+	public Transform target;
+	public float smoothSpeed = 0.25f;
+	public Vector3 offset;
+	private Vector3 previousPosition;
+	void Update()
+	{
+		previousPosition = target.position;
+	}
+
+	void LateUpdate()
+	{
+		offset = Quaternion.AngleAxis (target.position.x * smoothSpeed, Vector3.up) * offset;
+		if (previousPosition != target.position) {
+			//transform.position = target.position + offset;
+		}
+		transform.LookAt (target);
+	}
+}
---
+++ CabooseGame/Assets/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	public float speed = 260f;
+	Vector3 movement;
+	Animator anim;
+	Rigidbody playerRigidbody;
+	int floorMask;
+	private KeyCode previousKey;
+
+	void Awake() 
+	{
+		floorMask = LayerMask.GetMask ("Floor");
+		anim = GetComponent<Animator> ();
+		playerRigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate() 
+	{
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+
+		Move (h, v);
+		Rotate ();
+		Animating (h, v);
+	}
+
+	void Move(float h, float v)
+	{
+		movement = new Vector3(h, 0f, v) * speed * Time.deltaTime;
+
+		if (isWalking (h, v)) {
+			movement = transform.forward * speed * Time.deltaTime;
+		}
+
+		playerRigidbody.MovePosition (transform.position + movement);
+	}
+
+	void Rotate()
+	{
+		// Rotate right
+		if (Input.GetKey (KeyCode.X)) {
+			transform.Rotate(Vector3.up * speed * Time.deltaTime);
+		} else if (Input.GetKey (KeyCode.Z)) { // Rotate left
+			transform.Rotate(-Vector3.up * speed * Time.deltaTime);
+		}
+	}
+
+	void Animating(float h, float v)
+	{
+		anim.SetBool ("isWalking", isWalking(h, v));
+
+		if (Input.GetKey (KeyCode.Space)) {
+			anim.SetTrigger ("jump");
+		}
+	}
+
+	private bool isWalking(float h, float v) 
+	{
+		if (h != 0f || v != 0f)
+			return true;
+		else
+			return false;
+	}
+}
