0c7783d590b7b0b5347e41c56f4790070d2d45e7
Joe Bruno <bruno72@me.com>
2017-12-18 01:07:19 -0500

    Added music and skybox.

---
+++ CabooseGame/Assets/AudioPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour {
+
+    public AudioSource audioSource;
+    
+	// Use this for initialization
+	void Start () {
+        audioSource.loop = true;
+        audioSource.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
